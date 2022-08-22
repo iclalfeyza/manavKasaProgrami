866480adec6cd97076ed485e7a55cbe245266671
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-08-22 22:02:42 +0300

    manavKasaProgrami.java

--- src/meyve.java
+++ src/meyve.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class meyve {
     public static void main(String[] args) {
         double armut=2.14, elma=3.67, domates=1.11, muz=0.95, patlican=5;
